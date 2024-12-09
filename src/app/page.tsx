4076c8807cea7cbf22289a23e99b0334708eee7a
ImranQamarUddin <59954011+ImranQamarUddin@users.noreply.github.com>
2024-12-09 09:20:00 +0500

    complete

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from './components/hero'
+import Hero1 from './components/hero1'
+import Hero2 from './components/hero2'
+import Hero4 from './components/hero4'
+
+const page = () => {
+  return (
+    <div>
+      <Hero/>
+      <Hero1/>
+      <Hero2/>
+      
+      <Hero4/>
+    </div>
+  )
+}
+
+export default page
